30809541fcef9d44e0bffe8c599f7611e40717e1
Uftadi <akif.gueler@dci.education>
2024-01-07 20:12:50 +0100

    add url

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,193 +4,204 @@ import axios from "axios";
 const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
-    const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState([]);
 
-    const [nameInput, setNameInput] = useState("");
-    const [produkten, setProdukten] = useState("");
-    const [steps, setSteps] = useState("");
-    const [time, setTime] = useState(0);
-    const [difficulty, setDifficulty] = useState("");
-    const [selectedRecipeId, setSelectedRecipeId] = useState(null);
-    const [selectedUserId, setselectedUserId] = useState(null);
-    const [image, setImage] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [isEditingRecipe, setIsEditingRecipe] = useState(false);
-    const [isAddingRecipe, setIsAddingRecipe] = useState(false);
-    const [isRecipeUpdated, setIsRecipeUpdated] = useState(false);
-    const [shareMenuVisible, setShareMenuVisible] = useState(false);
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
+	const [nameInput, setNameInput] = useState("");
+	const [produkten, setProdukten] = useState("");
+	const [steps, setSteps] = useState("");
+	const [time, setTime] = useState(0);
+	const [difficulty, setDifficulty] = useState("");
+	const [selectedRecipeId, setSelectedRecipeId] = useState(null);
+	const [selectedUserId, setselectedUserId] = useState(null);
+	const [image, setImage] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [isEditingRecipe, setIsEditingRecipe] = useState(false);
+	const [isAddingRecipe, setIsAddingRecipe] = useState(false);
+	const [isRecipeUpdated, setIsRecipeUpdated] = useState(false);
+	const [shareMenuVisible, setShareMenuVisible] = useState(false);
+	const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-    const url = "http://localhost:3001";
+	const url = "https://rezept-share-plattform.onrender.com";
 
-    const fetchUserData = async () => {
-        try {
-          const response = await axios.get(url);
-          setUsers(response.data);
-          console.log(response.data);
-        } catch (error) {
-          console.error("Fehler beim Abrufen der Daten:", error.message);
-        }
-        setIsLoading(true);
-      };
+	const fetchUserData = async () => {
+		try {
+			const response = await axios.get(url);
+			setUsers(response.data);
+			console.log(response.data);
+		} catch (error) {
+			console.error("Fehler beim Abrufen der Daten:", error.message);
+		}
+		setIsLoading(true);
+	};
 
-      const updateRecipe = (orderId, recipeItemId) => {
-        const selectedOrder = users.find((o) => o._id === orderId);
-        if (selectedOrder) {
-          const selectedRecipe = selectedOrder.recipes.find(
-            (r) => r._id === recipeItemId
-          );
-    
-          if (selectedRecipe) {
-            setNameInput(selectedRecipe.title);
-            setProdukten(selectedRecipe.ingredients);
-            setSteps(selectedRecipe.steps);
-            setTime(selectedRecipe.time);
-            setDifficulty(selectedRecipe.difficulty);
-            setImage(selectedRecipe.image)
-            setSelectedRecipeId(recipeItemId);
-            setselectedUserId(orderId);
-            setIsEditingRecipe(true);
-          }
-        }
-      };
+	const updateRecipe = (orderId, recipeItemId) => {
+		const selectedOrder = users.find((o) => o._id === orderId);
+		if (selectedOrder) {
+			const selectedRecipe = selectedOrder.recipes.find(
+				(r) => r._id === recipeItemId
+			);
 
-      const saveOrder = async (e) => {
-        e.preventDefault();
-        try {
-          const newRecipeItem = {
-            title: nameInput,
-            ingredients: produkten,
-            steps: steps,
-            time: time,
-            difficulty: difficulty,
-            image: image
-          };
-    
-          if (selectedUserId && selectedRecipeId) {
-            const selectedUser = users.find((user) => user._id === selectedUserId);
-    
-            if (selectedUser) {
-              const selectedRecipeIndex = selectedUser.recipes.findIndex(
-                (recipe) => recipe._id === selectedRecipeId
-              );
-    
-              if (selectedRecipeIndex !== -1) {
-                selectedUser.recipes[selectedRecipeIndex] = newRecipeItem;
-    
-                await axios.put(
-                  `${url}/${selectedUserId}/${selectedRecipeId}`,
-                  newRecipeItem
-                );
-              }
-            }
-          } else {
-            await axios.post(url, newRecipeItem);
-          }
-    
-          fetchUserData();
-    
-          setNameInput("");
-          setProdukten("");
-          setSteps("");
-          setTime(0);
-          setDifficulty("");
-          setImage("");
-          setSelectedRecipeId(null);
-          setselectedUserId(null);
-          setIsAddingRecipe(false);
-          setIsEditingRecipe(false);
-        } catch (error) {
-          console.error("Fehler beim Speichern der Bestellung:", error.message);
-        }
-      };
+			if (selectedRecipe) {
+				setNameInput(selectedRecipe.title);
+				setProdukten(selectedRecipe.ingredients);
+				setSteps(selectedRecipe.steps);
+				setTime(selectedRecipe.time);
+				setDifficulty(selectedRecipe.difficulty);
+				setImage(selectedRecipe.image);
+				setSelectedRecipeId(recipeItemId);
+				setselectedUserId(orderId);
+				setIsEditingRecipe(true);
+			}
+		}
+	};
 
-      const deleteOrder = async (userId, recipeItemId) => {
-        try {
-          await axios.delete(`${url}/${userId}/${recipeItemId}`);
-          fetchUserData();
-        } catch (error) {
-          console.error("Fehler beim Löschen der Bestellung:", error.message);
-        }
-      };
+	const saveOrder = async (e) => {
+		e.preventDefault();
+		try {
+			const newRecipeItem = {
+				title: nameInput,
+				ingredients: produkten,
+				steps: steps,
+				time: time,
+				difficulty: difficulty,
+				image: image,
+			};
 
-      const shareOnFacebook = () => {
-        try {
-          const recipeDetails = `Name: ${selectedRecipe.title}\nZutaten: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
-          const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-            window.location.href
-          )}&quote=${encodeURIComponent(recipeDetails)}`;
-          window.open(shareUrl, "_blank");
-        } catch (error) {
-          console.error("Error sharing on Facebook:", error);
-        }
-      };
-    
-      const shareOnTwitter = () => {
-        try {
-          const tweetText = `Name: ${selectedRecipe.title}\nIngredients: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
-          const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-            tweetText
-          )}`;
-          window.open(tweetUrl, "_blank");
-        } catch (error) {
-          console.error("Error sharing on Twitter:", error);
-        }
-      };
+			if (selectedUserId && selectedRecipeId) {
+				const selectedUser = users.find((user) => user._id === selectedUserId);
 
-      const toggleShareMenu = (recipe) => {
-        setShareMenuVisible(!shareMenuVisible);
-        setSelectedRecipe(recipe);
-      };
+				if (selectedUser) {
+					const selectedRecipeIndex = selectedUser.recipes.findIndex(
+						(recipe) => recipe._id === selectedRecipeId
+					);
 
-      const ShareMenu = ({ onShareFacebook, onShareTwitter }) => {
-        return (
-          <div className="share-menu">
-            <p>Where would you like to share your recipes?</p>
-            <button onClick={onShareFacebook}>Facebook</button>
-            <br />
-      
-            <button onClick={onShareTwitter}>Twitter</button>
-          </div>
-        );
-      };
-      
+					if (selectedRecipeIndex !== -1) {
+						selectedUser.recipes[selectedRecipeIndex] = newRecipeItem;
 
+						await axios.put(
+							`${url}/${selectedUserId}/${selectedRecipeId}`,
+							newRecipeItem
+						);
+					}
+				}
+			} else {
+				await axios.post(url, newRecipeItem);
+			}
 
-      return (
+			fetchUserData();
+
+			setNameInput("");
+			setProdukten("");
+			setSteps("");
+			setTime(0);
+			setDifficulty("");
+			setImage("");
+			setSelectedRecipeId(null);
+			setselectedUserId(null);
+			setIsAddingRecipe(false);
+			setIsEditingRecipe(false);
+		} catch (error) {
+			console.error("Fehler beim Speichern der Bestellung:", error.message);
+		}
+	};
+
+	const deleteOrder = async (userId, recipeItemId) => {
+		try {
+			await axios.delete(`${url}/${userId}/${recipeItemId}`);
+			fetchUserData();
+		} catch (error) {
+			console.error("Fehler beim Löschen der Bestellung:", error.message);
+		}
+	};
+
+	const shareOnFacebook = () => {
+		try {
+			const recipeDetails = `Name: ${selectedRecipe.title}\nZutaten: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
+			const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+				window.location.href
+			)}&quote=${encodeURIComponent(recipeDetails)}`;
+			window.open(shareUrl, "_blank");
+		} catch (error) {
+			console.error("Error sharing on Facebook:", error);
+		}
+	};
+
+	const shareOnTwitter = () => {
+		try {
+			const tweetText = `Name: ${selectedRecipe.title}\nIngredients: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
+			const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+				tweetText
+			)}`;
+			window.open(tweetUrl, "_blank");
+		} catch (error) {
+			console.error("Error sharing on Twitter:", error);
+		}
+	};
+
+	const toggleShareMenu = (recipe) => {
+		setShareMenuVisible(!shareMenuVisible);
+		setSelectedRecipe(recipe);
+	};
+
+	const ShareMenu = ({ onShareFacebook, onShareTwitter }) => {
+		return (
+			<div className="share-menu">
+				<p>Where would you like to share your recipes?</p>
+				<button onClick={onShareFacebook}>Facebook</button>
+				<br />
+
+				<button onClick={onShareTwitter}>Twitter</button>
+			</div>
+		);
+	};
+
+	return (
 		<UserContext.Provider
-			value={ {
-				        users, 
-                setUsers,
-                fetchUserData,
-                url,
-                updateRecipe,
-                saveOrder,
-                deleteOrder,
-                isLoading, setIsLoading,
-                isEditingRecipe, setIsEditingRecipe,
-                isAddingRecipe, setIsAddingRecipe,
-                nameInput, setNameInput,
-                produkten, setProdukten,
-                steps, setSteps,
-                time, setTime,
-                difficulty, setDifficulty,
-                selectedRecipeId, setSelectedRecipeId,
-                selectedUserId, setselectedUserId,
-                image, setImage,
-                isRecipeUpdated, setIsRecipeUpdated,
-                toggleShareMenu,
-                shareOnFacebook, shareOnTwitter,
-                shareMenuVisible, setShareMenuVisible,
-                selectedRecipe, setSelectedRecipe,
-                ShareMenu
-
-
+			value={{
+				users,
+				setUsers,
+				fetchUserData,
+				url,
+				updateRecipe,
+				saveOrder,
+				deleteOrder,
+				isLoading,
+				setIsLoading,
+				isEditingRecipe,
+				setIsEditingRecipe,
+				isAddingRecipe,
+				setIsAddingRecipe,
+				nameInput,
+				setNameInput,
+				produkten,
+				setProdukten,
+				steps,
+				setSteps,
+				time,
+				setTime,
+				difficulty,
+				setDifficulty,
+				selectedRecipeId,
+				setSelectedRecipeId,
+				selectedUserId,
+				setselectedUserId,
+				image,
+				setImage,
+				isRecipeUpdated,
+				setIsRecipeUpdated,
+				toggleShareMenu,
+				shareOnFacebook,
+				shareOnTwitter,
+				shareMenuVisible,
+				setShareMenuVisible,
+				selectedRecipe,
+				setSelectedRecipe,
+				ShareMenu,
 			}}
 		>
 			{children}
 		</UserContext.Provider>
-	);  
-}
+	);
+};
 
 export { UserContext, UserContextProvider };
--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -5,186 +5,177 @@ import UserProfile from "../components/UserProfile";
 import { UserContext } from "../context/UserContext";
 import UserRecipes from "../components/UserRecipes";
 
-
- const url = "http://localhost:3001";
-
 const ShareMenu = ({ onShareFacebook, onShareTwitter }) => {
-  return (
-    <div className="share-menu">
-      <p>Where would you like to share your recipes?</p>
-      <button onClick={onShareFacebook}>Facebook</button>
-      <br />
+	return (
+		<div className="share-menu">
+			<p>Where would you like to share your recipes?</p>
+			<button onClick={onShareFacebook}>Facebook</button>
+			<br />
 
-      <button onClick={onShareTwitter}>Twitter</button>
-    </div>
-  );
+			<button onClick={onShareTwitter}>Twitter</button>
+		</div>
+	);
 };
 
 const User = () => {
-  
-  // useEffect(() => {
-  //   fetchUserData();
-  // }, []);
+	// useEffect(() => {
+	//   fetchUserData();
+	// }, []);
 
+	const { users, setUsers, fetchUserData, url } = useContext(UserContext);
 
-  const {users, setUsers, fetchUserData, url} = useContext(UserContext);
+	const [shareMenuVisible, setShareMenuVisible] = useState(false);
+	const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-  const [shareMenuVisible, setShareMenuVisible] = useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
+	// const fetchData = async () => {
+	//   try {
+	//     const response = await axios.get(url);
+	//     setUsers(response.data);
+	//     console.log(response.data);
+	//   } catch (error) {
+	//     console.error("Fehler beim Abrufen der Daten:", error.message);
+	//   }
+	// };
 
+	useEffect(() => {
+		fetchUserData();
+	}, []);
 
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await axios.get(url);
-  //     setUsers(response.data);
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Fehler beim Abrufen der Daten:", error.message);
-  //   }
-  // };
+	const updateRecipe = (orderId, recipeItemId) => {
+		const selectedOrder = users.find((o) => o._id === orderId);
+		if (selectedOrder) {
+			const selectedRecipe = selectedOrder.recipes.find(
+				(r) => r._id === recipeItemId
+			);
 
-  useEffect(() => {
-    fetchUserData();
-  }, []);
+			if (selectedRecipe) {
+				setNameInput(selectedRecipe.title);
+				setProdukten(selectedRecipe.ingredients);
+				setSteps(selectedRecipe.steps);
+				setTime(selectedRecipe.time);
+				setDifficulty(selectedRecipe.difficulty);
+				setSelectedRecipeId(recipeItemId);
+				setselectedUserId(orderId);
+			}
+		}
+	};
 
-  const updateRecipe = (orderId, recipeItemId) => {
-    const selectedOrder = users.find((o) => o._id === orderId);
-    if (selectedOrder) {
-      const selectedRecipe = selectedOrder.recipes.find(
-        (r) => r._id === recipeItemId
-      );
+	const saveOrder = async () => {
+		try {
+			const newRecipeItem = {
+				title: nameInput,
+				ingredients: produkten,
+				steps: steps,
+				time: time,
+				difficulty: difficulty,
+				image: image,
+			};
+			const missingFields = [];
 
-      if (selectedRecipe) {
-        setNameInput(selectedRecipe.title);
-        setProdukten(selectedRecipe.ingredients);
-        setSteps(selectedRecipe.steps);
-        setTime(selectedRecipe.time);
-        setDifficulty(selectedRecipe.difficulty);
-        setSelectedRecipeId(recipeItemId);
-        setselectedUserId(orderId);
-      }
-    }
-  };
+			if (nameInput === "") {
+				missingFields.push("Titel");
+			}
+			if (produkten === "") {
+				missingFields.push("Zutaten");
+			}
+			if (steps === "") {
+				missingFields.push("Schritte");
+			}
+			if (time === "") {
+				missingFields.push("Zeit");
+			}
+			if (difficulty === "") {
+				missingFields.push("Schwierigkeitsgrad");
+			}
 
-  const saveOrder = async () => {
-    try {
-      const newRecipeItem = {
-        title: nameInput,
-        ingredients: produkten,
-        steps: steps,
-        time: time,
-        difficulty: difficulty,
-        image: image
-      };
-      const missingFields = [];
+			if (missingFields.length > 0) {
+				const missingFieldsString = missingFields.join(", ");
+				alert(
+					`Folgende Felder müssen ausgefüllt werden: ${missingFieldsString}`
+				);
+				return;
+			}
 
-    if (nameInput === "") {
-      missingFields.push("Titel");
-    }
-    if (produkten === "") {
-      missingFields.push("Zutaten");
-    }
-    if (steps === "") {
-      missingFields.push("Schritte");
-    }
-    if (time === "") {
-      missingFields.push("Zeit");
-    }
-    if (difficulty === "") {
-      missingFields.push("Schwierigkeitsgrad");
-    }
+			if (selectedUserId && selectedRecipeId) {
+				const selectedUser = users.find((user) => user._id === selectedUserId);
 
-    if (missingFields.length > 0) {
-      const missingFieldsString = missingFields.join(', ');
-      alert(`Folgende Felder müssen ausgefüllt werden: ${missingFieldsString}`);
-      return;
-    }
+				if (selectedUser) {
+					const selectedRecipeIndex = selectedUser.recipes.findIndex(
+						(recipe) => recipe._id === selectedRecipeId
+					);
 
-      if (selectedUserId && selectedRecipeId) {
-        const selectedUser = users.find((user) => user._id === selectedUserId);
+					if (selectedRecipeIndex !== -1) {
+						selectedUser.recipes[selectedRecipeIndex] = newRecipeItem;
 
-        if (selectedUser) {
-          const selectedRecipeIndex = selectedUser.recipes.findIndex(
-            (recipe) => recipe._id === selectedRecipeId
-          );
+						await axios.put(
+							`${url}/${selectedUserId}/${selectedRecipeId}`,
+							newRecipeItem
+						);
+					}
+				}
+			} else {
+				await axios.post(url, newRecipeItem);
+			}
 
-          if (selectedRecipeIndex !== -1) {
-            selectedUser.recipes[selectedRecipeIndex] = newRecipeItem;
+			fetchUserData();
 
-            await axios.put(
-              `${url}/${selectedUserId}/${selectedRecipeId}`,
-              newRecipeItem
-            );
-          }
-        }
-      } else {
-        await axios.post(url, newRecipeItem);
-      }
+			setNameInput("");
+			setProdukten("");
+			setSteps("");
+			setTime(0);
+			setDifficulty("");
+			setSelectedRecipeId(null);
+			setselectedUserId(null);
+		} catch (error) {
+			console.error("Fehler beim Speichern der Bestellung:", error.message);
+		}
+	};
 
-      fetchUserData();
+	const deleteOrder = async (userId, recipeItemId) => {
+		try {
+			await axios.delete(`${url}/${userId}/${recipeItemId}`);
+			fetchUserData();
+		} catch (error) {
+			console.error("Fehler beim Löschen der Bestellung:", error.message);
+		}
+	};
 
-      setNameInput("");
-      setProdukten("");
-      setSteps("");
-      setTime(0);
-      setDifficulty("");
-      setSelectedRecipeId(null);
-      setselectedUserId(null);
-    } catch (error) {
-      console.error("Fehler beim Speichern der Bestellung:", error.message);
-    }
-  };
+	const shareOnFacebook = () => {
+		try {
+			const recipeDetails = `Name: ${selectedRecipe.title}\nZutaten: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
+			const shareUrl = `https://www.facebook.com/sharer/sharer.php?quote=${encodeURIComponent(
+				recipeDetails
+			)}`;
+			window.open(shareUrl, "_blank");
+		} catch (error) {
+			console.error("Error sharing on Facebook:", error);
+		}
+	};
 
-  const deleteOrder = async (userId, recipeItemId) => {
-    try {
-      await axios.delete(`${url}/${userId}/${recipeItemId}`);
-      fetchUserData();
-    } catch (error) {
-      console.error("Fehler beim Löschen der Bestellung:", error.message);
-    }
-  };
+	const shareOnTwitter = () => {
+		try {
+			const tweetText = `Name: ${selectedRecipe.title}\nIngredients: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
+			const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+				tweetText
+			)}`;
+			window.open(tweetUrl, "_blank");
+		} catch (error) {
+			console.error("Error sharing on Twitter:", error);
+		}
+	};
 
-  const shareOnFacebook = () => {
-    try {
-        const recipeDetails = `Name: ${selectedRecipe.title}\nZutaten: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
-        const shareUrl = `https://www.facebook.com/sharer/sharer.php?quote=${encodeURIComponent(recipeDetails)}`;
-        window.open(shareUrl, "_blank");
-    } catch (error) {
-        console.error("Error sharing on Facebook:", error);
-    }
-};
+	const toggleShareMenu = (recipe) => {
+		setShareMenuVisible(!shareMenuVisible);
+		setSelectedRecipe(recipe);
+	};
 
-
-
-
-  const shareOnTwitter = () => {
-    try {
-      const tweetText = `Name: ${selectedRecipe.title}\nIngredients: ${selectedRecipe.ingredients}\nTime: ${selectedRecipe.time}\nDifficulty: ${selectedRecipe.difficulty}`;
-      const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-        tweetText
-      )}`;
-      window.open(tweetUrl, "_blank");
-    } catch (error) {
-      console.error("Error sharing on Twitter:", error);
-    }
-  };
-
-  const toggleShareMenu = (recipe) => {
-    setShareMenuVisible(!shareMenuVisible);
-    setSelectedRecipe(recipe);
-  };
-
-
-  return (
-    <>
-    <UpperNav />
-    <UserProfile />
-    <UserRecipes />
-    </>
-  );
+	return (
+		<>
+			<UpperNav />
+			<UserProfile />
+			<UserRecipes />
+		</>
+	);
 };
 
 export default User;
-
-
-
